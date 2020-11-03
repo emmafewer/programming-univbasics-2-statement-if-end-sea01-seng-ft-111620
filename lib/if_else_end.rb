edb186d18e94e2e46b5adbddeb3bc41cb06ce0ca
Emma Fewer <emmafewer20@gmail.com>
2020-11-03 18:34:49 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 current_time = Time.now.to_i
 if current_time.even?
@@ -5,3 +6,12 @@ if current_time.even?
 else
  puts "Odd!"
 end
+=======
+# Write your solution here
+run_code_inside = FALSE
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
+>>>>>>> 72959fbdae23335033f3f57b3cd45fd06da7a41e
